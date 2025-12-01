4ec805adaaa774d48ea40fef2929e6f76de38638
mykushyn <ni.kushynov@gmail.com>
2025-12-01 22:14:36 +0100

    Add files via upload

---
+++ AI_pws-main/WebApplication0.0/Pages/Intro.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication0._0.Pages
+{
+    public class IntroModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
